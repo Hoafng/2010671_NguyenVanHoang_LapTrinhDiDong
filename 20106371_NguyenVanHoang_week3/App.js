8c6df35d4b3a96f057c910be4c4d93375be00211
Hoang <hoangghcc@gmail.com>
2024-09-27 01:02:26 +0700

    finish Tuan3

---
+++ 20106371_NguyenVanHoang_week3/App.js
@@ -0,0 +1,28 @@
+import { Text, SafeAreaView, StyleSheet } from 'react-native';
+
+// You can import supported modules from npm
+import { Card } from 'react-native-paper';
+
+// or any files within the Snack
+import YourApp from './components/Screen1'
+
+export default function App() {
+  return ( 
+        <YourApp/>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
