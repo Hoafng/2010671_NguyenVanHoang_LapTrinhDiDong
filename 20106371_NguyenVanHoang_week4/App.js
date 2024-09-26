f021fd0b3f13d50a65ed14873113a025f2346198
Hoang <hoangghcc@gmail.com>
2024-09-27 01:17:04 +0700

    finish Tuan 4

---
+++ 20106371_NguyenVanHoang_week4/App.js
@@ -0,0 +1,14 @@
+import { Text, SafeAreaView, StyleSheet } from 'react-native';
+
+// You can import supported modules from npm
+import { Card } from 'react-native-paper';
+
+// or any files within the Snack
+import YourApp from './components/Tiki_ok';
+
+export default function App() {
+  return (
+        <YourApp />
+       
+  );
+}
